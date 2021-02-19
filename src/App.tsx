9875359c5bda66ce5764848708471d9d9d24c87a
GelatoMao <gelatomao@outlook.com>
2021-02-19 11:38:34 +0800

    完成底部导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import React from "react"
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom"
+import styled from "styled-components"
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+  flex-grow: 1;
+  /* 防止页面超出main区域 */
+  overflow: auto;
+`
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.3333%;
+      padding: 16px;
+      text-align: center;
+    }
+  }
+`
 function App() {
   return (
-    <div className="App">
-      haha
-    </div>
-  );
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
+  )
 }
 
-export default App;
+function Tags() {
+  return <h2>tags</h2>
+}
+
+function Money() {
+  return <h2>money</h2>
+}
+
+function Statistics() {
+  return <h2>statistics</h2>
+}
+
+function NoMatch() {
+  return <div>页面不存在 可能输错地址了</div>
+}
+
+export default App
